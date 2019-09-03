8fe00783ca24657306ad2957b4b4fe35b0737a63
LeonovEgor <leonov.egor@gmail.com>
2019-09-03 18:46:30 +0300

    Добавление базового кода задания

---
+++ src/Lesson5/Game/Race.java
@@ -0,0 +1,14 @@
+package Lesson5.Stages;
+
+import Lesson5.Stages.Stage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
